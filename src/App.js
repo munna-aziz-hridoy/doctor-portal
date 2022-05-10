f25a13a3682f8c2462acdb421a85d655b1970dae
munna-aziz-hridoy <munna.aziz.hridoy@gmail.com>
2022-05-11 00:40:25 +0800

    create doctor portal app install dependency and create header with all initial setup

--- src/App.js
+++ src/App.js
@@ -1,24 +1,27 @@
-import logo from './logo.svg';
-import './App.css';
+import { Route, Routes } from "react-router-dom";
+import Header from "./Components/Header/Header";
+import About from "./Pages/About/About";
+import Appointment from "./Pages/Appointment/Appointment";
+import Contact from "./Pages/Contact/Contact";
+import Home from "./Pages/Home/Home";
+import Login from "./Pages/Login/Login";
+import Register from "./Pages/Register/Register";
+import Reviews from "./Pages/Reviews/Reviews";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+      <Header />
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/about" element={<About />} />
+        <Route path="/appointment" element={<Appointment />} />
+        <Route path="/reviews" element={<Reviews />} />
+        <Route path="/contact" element={<Contact />} />
+        <Route path="/login" element={<Login />} />
+        <Route path="/register" element={<Register />} />
+      </Routes>
+    </>
   );
 }
 
---
+++ src/Components/Header/Header.jsx
@@ -0,0 +1,64 @@
+import React from "react";
+import { Link } from "react-router-dom";
+
+const Header = () => {
+  const menuItems = (
+    <>
+      <li className="capitalize hover:bg-accent hover:text-slate-100 duration-200 rounded-lg">
+        <Link to="/">home</Link>
+      </li>
+      <li className="capitalize hover:bg-accent hover:text-slate-100 duration-200 rounded-lg">
+        <Link to="/about">about</Link>
+      </li>
+      <li className="capitalize hover:bg-accent hover:text-slate-100 duration-200 rounded-lg">
+        <Link to="/appointment">appointment</Link>
+      </li>
+      <li className="capitalize hover:bg-accent hover:text-slate-100 duration-200 rounded-lg">
+        <Link to="/reviews">reviews</Link>
+      </li>
+      <li className="capitalize hover:bg-accent hover:text-slate-100 duration-200 rounded-lg">
+        <Link to="/contact">contact us</Link>
+      </li>
+      <li className="capitalize hover:bg-accent hover:text-slate-100 duration-200 rounded-lg">
+        <Link to="/login">login</Link>
+      </li>
+    </>
+  );
+
+  return (
+    <div className="navbar bg-base-100 justify-between h-16">
+      <div className="navbar-start w-full lg:w-fit justify-between">
+        <div className="dropdown">
+          <label tabIndex="0" className="btn btn-ghost lg:hidden">
+            <svg
+              xmlns="http://www.w3.org/2000/svg"
+              className="h-5 w-5"
+              fill="none"
+              viewBox="0 0 24 24"
+              stroke="currentColor"
+            >
+              <path
+                strokeLinecap="round"
+                strokeLinejoin="round"
+                strokeWidth="2"
+                d="M4 6h16M4 12h8m-8 6h16"
+              />
+            </svg>
+          </label>
+          <ul
+            tabIndex="0"
+            className="menu menu-compact dropdown-content mt-3 p-2 shadow bg-base-100 rounded-box w-52 gap-2"
+          >
+            {menuItems}
+          </ul>
+        </div>
+        <a className="btn btn-ghost text-xl capitalize">doctor portal</a>
+      </div>
+      <div className="navbar-center hidden lg:flex">
+        <ul className="menu menu-horizontal p-0 gap-4">{menuItems}</ul>
+      </div>
+    </div>
+  );
+};
+
+export default Header;
